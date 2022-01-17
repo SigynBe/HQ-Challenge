48c0d126d81c7abc3ddf7e25bb0f62af73358a53
@rebeca.correa <rebeca.correa@partners.mv.com.br>
2022-01-17 08:35:54 -0400

    Initial Commit

---
+++ HQ challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HQ challenge
+//
+//  Created by Rebeca Pacheco on 14/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HQ challenge/HQ_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HQ_challengeApp.swift
+//  HQ challenge
+//
+//  Created by Rebeca Pacheco on 14/01/22.
+//
+
+import SwiftUI
+
+@main
+struct HQ_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
